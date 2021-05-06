e9eade5f676724bae089c54e855fcc125b4e4325
mariaDulesova <maria.dulesova@gmail.com>
2021-05-06 17:55:34 +0200

    update with css

--- js/script.js
+++ js/script.js
@@ -8,6 +8,9 @@
 // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
+
+
+//Definisco 5 Random Numbers
 var randomNumber =[];
 
 while (randomNumber.length < 5) {
@@ -21,18 +24,19 @@ while (randomNumber.length < 5) {
 console.log(randomNumber);
 var numbersToMemorize = alert("Please remember the following numbers: " + randomNumber);
 
-var startCountdown = 10;
+//Gioco
+var startCountdown = 30;
 var countdown = setInterval(function(){
     
     var userNumberArray = [];
     if (startCountdown!=-1) {
-        document.getElementById("countdown").innerHTML = startCountdown;
-        startCountdown --; 
+        document.getElementById("countdown").innerHTML = "<p>" + startCountdown + "</p>"; 
+        startCountdown --; //aggiornamento il numero di countdown
     } else {
-        clearInterval(countdown);
+        clearInterval(countdown); // fermo countdown una volta raggiunto lo 0;
 
         for (var i = 0; i < 5; i++){
-            var userNumber = parseInt(prompt("Please insert the numbers you remember:"));
+            var userNumber = parseInt(prompt("Please insert the numbers you remember:")); //chiedo all'utente 5 volte il numero. Lo salvo solo se e' presente nel randomNumber;
             console.log(userNumber);
             if (isInArray(userNumber, randomNumber)){
                 userNumberArray.push(userNumber);
